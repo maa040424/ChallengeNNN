3f35cd33078effb4062d1ff4122ce8b598a3003f
Aqueit <alghifarimadam04@gmail.com>
2024-11-01 20:09:13 +0800

    Kalkulator py

---
+++ Hari01/Python/kalkulator.py
@@ -0,0 +1,13 @@
+def kalkulator(a, b, operator):
+    if operator == '+':
+        return a + b
+    elif operator == '-':
+        return a - b
+    elif operator == '*':
+        return a * b
+    elif operator == '/':
+        return a / b
+    else:
+        return "Operator tidak valid"
+
+print(kalkulator(10, 5, '+'))  

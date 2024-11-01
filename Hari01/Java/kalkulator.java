78abfdda2f208c5f4967a81910e23e83a4daceee
Aqueit <alghifarimadam04@gmail.com>
2024-11-01 20:11:03 +0800

    Kalkulator java

---
+++ Hari01/Java/kalkulator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Kalkulator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan angka pertama: ");
+        double num1 = input.nextDouble();
+
+        System.out.print("Masukkan operator (+, -, *, /): ");
+        char operator = input.next().charAt(0);
+
+        System.out.print("Masukkan angka kedua: ");
+        double num2 = input.nextDouble();
+
+        double result;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Operator tidak valid");
+                return;
+        }
+
+        System.out.println("Hasil: " + result);
+    }
+}

b0906484211e5e06f4f4ef9d005bb4c7d32b02d7
Aqueit <alghifarimadam04@gmail.com>
2024-11-03 19:58:30 +0800

    Hari 03

--- Hari03/Python/konversi_mata_uang.py
+++ Hari03/Python/konversi_mata_uang.py
@@ -17,6 +17,3 @@ for mata_uang, jumlah in hasil_konversi.items():
     print(f"{mata_uang}: {jumlah:.2f}")
     print(f"{mata_uang}: {jumlah:.2f}")
     print(f"{mata_uang}: {jumlah:.2f}")
-    print(f"{mata_uang}: {jumlah:.2f}")
-    print(f"{mata_uang}: {jumlah:.2f}")
-    print(f"{mata_uang}: {jumlah:.2f}")

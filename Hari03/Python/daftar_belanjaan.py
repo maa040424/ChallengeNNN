9aaf24eb3ecd6571631b08acbdb9e1e66a69dc8e
Aqueit <alghifarimadam04@gmail.com>
2024-11-03 20:12:22 +0800

    Hari 03

--- Hari03/Python/daftar_belanjaan.py
+++ Hari03/Python/daftar_belanjaan.py
@@ -5,3 +5,10 @@ def daftar_belanjaan():
         print("\nMenu:")
         print("1. Tambah Belanjaan")
         print("2. Tampilkan Daftar Belanjaan")
+        print("3. Hapus Item")
+        print("4. Keluar")
+        
+        pilihan = input("Pilih opsi (1-4): ")
+        
+        if pilihan == "1" :
+            item = input("Masukan item belanjaan:")

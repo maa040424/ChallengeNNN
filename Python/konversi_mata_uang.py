96c53094844023283b45ce44a34545b685617183
Aqueit <alghifarimadam04@gmail.com>
2024-11-04 17:07:44 +0800

    Hari04

---
+++ Python/konversi_mata_uang.py
@@ -0,0 +1,17 @@
+def konversi_mata_uang(rupiah):
+    nilai_tukar = {
+        "USD":0.000066,  #dolar
+        "EUR":0.000057,  #Euro
+        "JPY":0.010      #Yen mun kd salah
+    }
+    hasil = {}
+    for mata_uang, kurs in nilai_tukar.items():
+        hasil[mata_uang] = rupiah * kurs
+        return hasil
+    
+rupiah = float(input("Masukkan Jumlah Rupiah: "))
+hasil_konversi = konversi_mata_uang(rupiah)
+
+print("Hasil Konversi: ")
+for mata_uang, jumlah in hasil_konversi.items():
+    print(f"{mata_uang}: {jumlah:.2f}")

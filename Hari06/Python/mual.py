e2d5afcd38904c8188817e2dadc907ce492d47b9
Aqueit <alghifarimadam04@gmail.com>
2024-11-06 20:46:24 +0800

    Hari 06

--- Hari06/Python/mual.py
+++ Hari06/Python/mual.py
@@ -20,3 +20,12 @@ def final_step(x, y, z):
         result = result * random.randint(1, 100)
     return result
 
+def mual_algorithm(a, b, c):
+    seq = [a, b, c]
+    for _ in range(15):
+        seq = [recursive_weird(x) for x in seq]  # Rekursi yang membingungkan
+        seq = [final_step(seq[0], seq[1], seq[2]) for seq in zip(seq, seq[1:], seq[2:])]  # Zipping yang rumit
+        seq = [x + random.randint(-5, 5) for x in seq]  # Mengacak hasil setiap iterasi
+    return sum(seq) - max(seq)
+
+

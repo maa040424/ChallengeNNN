eaa9ed202f7bd8f78b2bd6193bf27ce2ae11f4fd
Rahmad <Rahmadr473@gmail.com>
2024-11-28 21:27:47 +0800

    Cuannnn

---
+++ Bantu_orang/Rahmad/tugas1/lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Helloworld());
+}
+
+class Helloworld extends StatelessWidget {
+  const Helloworld({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Hello World",
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: const Text("Hello World App"),
+          centerTitle: true,
+          actions: [
+            IconButton(icon: Icon(Icons.search), onPressed: () {}),
+            IconButton(icon: Icon(Icons.settings), onPressed: () {})
+          ],
+          shape: const BeveledRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(25),
+              bottomRight: Radius.circular(25),
+            ),
+          ),
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Nama: Muhammad Adam Alghifari',
+                  style: TextStyle(fontSize: 18),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  'NPM: 2210010314',
+                  style: TextStyle(fontSize: 18),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  'Kelas : 5B BJB Reguler Pagi',
+                  style: TextStyle(fontSize: 18),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  'Semester: 5',
+                  style: TextStyle(fontSize: 18),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  'Tahun Angkatan: 2022',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+          ),
+        ),
+        drawer: Drawer(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notifications), label: 'Notifikasi'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: 'Pengaturan'),
+          ],
+        ),
+      ),
+    );
+  }
+}
